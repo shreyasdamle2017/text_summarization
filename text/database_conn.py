468978b6471959f6e49add22fbb89332ca95a67a
shreyasdamle2017 <62628641+shreyasdamle2017@users.noreply.github.com>
2020-10-23 19:38:10 -0400

    Add files via upload

---
+++ text/database_conn.py
@@ -0,0 +1,27 @@
+import psycopg2
+
+conn = psycopg2.connect(database="dbrolo3n8dbgrp", user="udqhymlpwxvcbb",
+                        password="f877f7ebc264d2771e60f278adca0bc266d0323a3a319134b93fce964c805766",
+                        host="ec2-54-236-146-234.compute-1.amazonaws.com",
+                        port="5432")
+
+print("Opened database successfully")
+
+
+cur = conn.cursor()
+
+cur.execute('''CREATE TABLE tbl_document
+(  document BYTEA,
+  username varchar,
+);''')
+
+
+print("Table created successfully")
+
+conn.commit()
+conn.close()
+
+
+
+
+
